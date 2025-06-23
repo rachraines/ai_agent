c07b6b60e14d0635a04223607f144f5cd446e8cd
rachraines <rachraines97@gmail.com>
2025-06-23 10:28:48 -0400

    option for verbose repsonse

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from google import genai
 from dotenv import load_dotenv
 import sys
+from google.genai import types
 
 
 def main():
@@ -14,16 +15,27 @@ def main():
         print("Usage: python3 main.py 'Your prompt here'")
         sys.exit(1)
 
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
+
+    # Checks for --verbose flag
+    verbose = "--verbose" in sys.argv
+
+    # Stores previous prompts as a list
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     # AI response to the prompt
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=prompt
+        contents=messages
     )
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Repsonse tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: '{user_prompt}'")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Repsonse tokens: {response.usage_metadata.candidates_token_count}")
+    
     print(f"Response: {response.text}")
 
 if __name__== "__main__":
