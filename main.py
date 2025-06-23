0869f04c34c76eba3c31358e48734349d85a75be
rachraines <rachraines97@gmail.com>
2025-06-23 10:10:45 -0400

    content is able to be user specified from terminal

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
 from google import genai
 from dotenv import load_dotenv
+import sys
 
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    # Checks if a prompt was given by user in terminal
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py 'Your prompt here'")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
+    # AI response to the prompt
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=prompt
     )
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
