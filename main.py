b64a6d27ee3665f31cf4d3351e90f6e2bc992b1f
rachraines <rachraines97@gmail.com>
2025-06-27 11:38:41 -0400

    main.py updated to include all functions options

--- main.py
+++ main.py
@@ -38,13 +38,10 @@ def main():
 def generate_content(client, messages, verbose):
     # AI response to the prompt
     system_prompt = """
-    You are a helpful AI coding agent.
-
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
     - List files and directories
-
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
     """
 
     response = client.models.generate_content(
