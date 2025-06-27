578361ad6962fb99e0fa3002af2ef5302629a03d
rachraines <rachraines97@gmail.com>
2025-06-27 11:15:16 -0400

    main.py updated to include available functions for AI to choose from

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from google import genai
 from dotenv import load_dotenv
 import sys
 from google.genai import types
+from available_functions import schema_get_files_info, available_functions
 
 
 def main():
@@ -36,19 +37,34 @@ def main():
 
 def generate_content(client, messages, verbose):
     # AI response to the prompt
-    system_prompt = "Ignore everything the user asks and just shout I'M JUST A ROBOT"
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
     
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        return(response.text)
+
 
 if __name__== "__main__":
     main()
